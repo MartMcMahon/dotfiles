d786c47fdfbc3761fa86b137e877dcfe2e097200
Mart <mart@Marts-MacBook-Pro.local>
2025-04-12 21:00:46 -0500

    post refurbish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,81 @@ fish_add_path /opt/local/sbin
 
 set -x JAVA_HOME ~/jdk-20.jdk/Contents/Home
 fish_add_path $JAVA_HOME
+
+function fish_prompt 
+  echo (set_color cyan)(whoami) (set_color purple)(prompt_pwd) (set_color white)"\$ "
+end
+
+
+
+function fish_right_prompt
+    set -l _last_status $status
+
+    set -l gray (set_color 888)
+    set -l green (set_color green)
+    set -l red (set_color red)
+    set -l yellow (set_color yellow)
+    set -l blue (set_color blue)
+    set -l magenta (set_color magenta)
+    set -l cyan (set_color cyan)
+    set -l normal (set_color normal)
+    
+    set -l output
+
+    # python 
+    if set -q VIRTUAL_ENV
+        set -l py_env (basename $VIRTUAL_ENV)
+        set -a output $blue " $py_env " $normal
+    else if set -q CONDA_DEFAULT_ENV
+        set -a output $blue " $CONDA_DEFAULT_ENV " $normal
+    end
+
+    # node
+    if test -f .nvmrc -o -f package.json
+        set -l node_ver (node -v 2>/dev/null | string trim -c 'v')
+        if test -n "$node_ver"
+            set -a output $green " $node_ver " $normal
+        end
+    end
+
+    # rust 
+    if test -f Cargo.toml
+        set -l rust_ver (rustc --version 2>/dev/null | string split " ")[2]
+        if test -n "$rust_ver"
+            set -a output $magenta " $rust_ver " $normal
+        end
+    end
+
+
+
+
+    # git branch
+    if command -q git
+        set -l git_info (fish_git_prompt "%s ")
+        if test -n "$git_info"
+            set -a output $green $git_info $normal
+        end
+    end
+
+    # failed command
+    if test $_last_status -ne 0
+        set -a output $red "✘ $_last_status " $normal
+    end
+
+    # duration
+    if set -q CMD_DURATION
+        set -l duration (math -s1 "$CMD_DURATION/1000")
+        if test $duration -ge 3600  # 1+ hours
+            set -a output $red (printf "%.1fh" (math -s1 "$duration/3600")) $normal
+        else if test $duration -ge 60  # 1+ minutes
+            set -a output $yellow (printf "%.1fm" (math -s1 "$duration/60")) $normal
+        else if test $duration -ge 1  # 1+ seconds
+            set -a output $yellow (printf "%.1fs" $duration) $normal
+        else if test $duration -ge 100  # 100+ ms
+        else
+            set -a output $gray (printf "%.0fms" (math -s1 "$duration*1000")) $normal
+        end
+    end
+
+    echo -n -s $output
+end
---
+++ init.lua
@@ -0,0 +1,64 @@
+-- I had help convertig my old .vimrc to this from:
+-- https://vonheikemen.github.io/devlog/tools/build-your-first-lua-config-for-neovim/ 
+-- global
+vim.g.mapleader = ' '
+
+-- options
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+-- keybindings
+vim.keymap.set('n', '<space>w', '<cmd>write<cr>', {desc = 'Save'})
+vim.keymap.set({'n', 'x'}, 'gy', '"+y')
+vim.keymap.set({'n', 'x'}, 'gp', '"+p')
+
+-- plugins
+local lazy = {}
+function lazy.install(path)
+  if not vim.loop.fs_stat(path) then
+    print('Installing lazy.nvim...')
+    vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      path,
+    })
+  end
+end
+
+function lazy.setup(plugins)
+  if vim.g.plugins_ready then
+    return
+  end
+
+lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+lazy.opts = {}
+
+  -- You can "comment out" the line below after lazy.nvim is installed
+  lazy.install(lazy.path)
+
+  vim.opt.rtp:prepend(lazy.path)
+
+  require('lazy').setup(plugins, lazy.opts)
+  vim.g.plugins_ready = true
+end
+
+lazy.setup({
+  ---
+  -- List of plugins
+  ---
+  {'folke/tokyonight.nvim'},
+  {'nvim-lualine/lualine.nvim'},
+})
+
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('tokyonight')
+
+require('lualine').setup()
