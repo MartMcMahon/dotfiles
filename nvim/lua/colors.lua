c150866f7e8774651030f0ee586bc058201f3bd9
Mart <themcman@gmail.com>
2025-06-03 17:20:29 -0500

    colors

--- nvim/lua/colors.lua
+++ nvim/lua/colors.lua
@@ -6,9 +6,20 @@ return {
       vim.cmd.colorscheme('edge')
     end
   },
+  -- {
+  --  "folke/tokyonight.nvim",
+  --   lazy = true,
+  --   ft = { "c", "cpp" },
+  --   config = function ()
+  --     vim.cmd.colorscheme('tokyonight-night')
+  --     vim.g.sonokai_style = 'maia'
+  --     vim.g.sonokai_better_performance = 1
+  --   end
+  -- }
 }
 
--- { 'tokyonight-night' },
--- { 'sonokai' }
+
+-- "folke/tokyonight.nvim",
+-- "sainnhe/sonokai"
 -- vim.g.sonokai_style = 'maia'
 -- vim.g.sonokai_better_performance = 1
